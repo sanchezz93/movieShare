efcd67a19a5b3d737c8b6c3b25a53946103d5dcf
Alejandro Sanchez <alejandro.s1993@gmail.com>
2016-01-28 00:29:06 +0100

    Deintegrated cocoapods, will add a new PodFile

--- Pods/Target Support Files/OMDBAPI/OMDBAPI-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_OMDBAPI : NSObject
-@end
-@implementation PodsDummy_OMDBAPI
-@end
--- Pods/Target Support Files/OMDBAPI/OMDBAPI-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double OMDBAPIVersionNumber;
-FOUNDATION_EXPORT const unsigned char OMDBAPIVersionString[];
-
